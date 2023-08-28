1d2bc65ea2b096d234cac9f2338b50f8ef3280b9
benboardley <bboard1202@gmail.com>
2023-08-28 16:41:32 -0400

    fixed contact me

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { useForm, ValidationError } from '@formspree/react';
 function App() {
-  const [state, handleSubmit] = useForm("mdoregpo");
-  if (state.succeeded) {
-      return <p>Thank you for contacting me!</p>;
-  }
+ // const [state, handleSubmit] = useForm("mdoregpo");
+ //if (state.succeeded) {
+ //     return <p>Thank you for contacting me!</p>;
+ // <form onSubmit={handleSubmit}>
+//disabled={state.submitting}}
     return(
         <div className="content-center w-5/8 sm:w-1/2 mb-10 bg-gray-600 h-auto rounded-lg box-border  bg-opacity-70 p-10">
             <h1 className="text-2xl font-bold underline mb-5">
@@ -13,10 +14,13 @@ function App() {
             <p className="">Email: bboardle@purdue.edu</p>
             <p className="">Phone: 574-400-3147</p>
             <p className="">GitHub: benboardley</p>
-            <form onSubmit={handleSubmit}>
+            <form
+            action="https://formspree.io/f/mdoregpo"
+            method="POST"
+            >
             <div className="flex flex-col">
                 <label for="name">Full Name</label>
-                <input id="name" type="name" name="email" className="text-black"></input>
+                <input id="name" type="name" name="name" className="text-black"></input>
             </div>
             <div className="flex flex-col">
                 <label for="email">Email</label>
@@ -38,7 +42,7 @@ function App() {
             // errors={state.errors}
             // />
             }
-            <input className="bg-gray-600 border-2 border-gray-800 w-40 mt-2 rounded-lg"type="submit" disabled={state.submitting} value="Send"></input>
+            <input className="bg-gray-600 border-2 border-gray-800 w-40 mt-2 rounded-lg"type="submit" value="Send"></input>
         </form>
         </div>
     );
